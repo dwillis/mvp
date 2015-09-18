79ca59a80717cd7f56d8eec0d2e381e9dc96dbaa
Derek Willis <dwillis@gmail.com>
2015-09-18 11:26:25 -0400

    added script with 5 minute loop

---
+++ mvp.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'post_haste'
+require 'csv'
+include PostHaste
+
+loop do
+  mv_web = MostViewed.all('web', 100)
+  mv_mobile = MostViewed.all('mobile', 100)
+  CSV.open('csv/results.csv', 'a') do |row|
+    mv_web.each do |mv|
+      row << [mv.datetime.to_date, mv.platform, mv.type, mv.datetime, mv.title, mv.byline, mv.url, mv.rank]
+    end
+    mv_mobile.each do |mv|
+      row << [mv.datetime.to_date, mv.platform, mv.type, mv.datetime, mv.title, mv.byline, mv.url, mv.rank]
+    end
+  end
+  system 'git add csv/results.csv'
+  system "git commit -m 'updated from #{mv_web.first.datetime.to_s}'"
+  system 'git push origin master'
+  sleep(300)
+end
