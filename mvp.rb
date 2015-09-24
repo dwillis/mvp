1f7eeb33a8156dbf3a50442cbd246181b489ad9f
Derek Willis <dwillis@gmail.com>
2015-09-23 22:38:57 -0400

    ignore empty json

--- mvp.rb
+++ mvp.rb
@@ -9,23 +9,25 @@ loop do
   mv_web = MostViewed.all('web', 100)
   mv_mobile = MostViewed.all('mobile', 100)
   file_name = "csv/#{date}.csv"
-  begin
-    f = File.open(file_name)
-    wh = f.size > 50 ? false : true
-    f.close
-  rescue
-    wh = true
-  end
-  CSV.open(file_name, "a+", :write_headers => wh, :headers => ['date','platform','type','datetime','title','byline','url','rank']) do |row|
-    mv_web.each do |mv|
-      row << [mv.datetime.to_date, mv.platform, mv.type, mv.datetime, mv.title, mv.byline, mv.url, mv.rank]
+  unless mv_web.empty?
+    begin
+      f = File.open(file_name)
+      wh = f.size > 50 ? false : true
+      f.close
+    rescue
+      wh = true
     end
-    mv_mobile.each do |mv|
-      row << [mv.datetime.to_date, mv.platform, mv.type, mv.datetime, mv.title, mv.byline, mv.url, mv.rank]
+    CSV.open(file_name, "a+", :write_headers => wh, :headers => ['date','platform','type','datetime','title','byline','url','rank']) do |row|
+      mv_web.each do |mv|
+        row << [mv.datetime.to_date, mv.platform, mv.type, mv.datetime, mv.title, mv.byline, mv.url, mv.rank]
+      end
+      mv_mobile.each do |mv|
+        row << [mv.datetime.to_date, mv.platform, mv.type, mv.datetime, mv.title, mv.byline, mv.url, mv.rank]
+      end
     end
+    system 'git add csv/'
+    system "git commit -m 'updated from #{mv_web.first.datetime.to_s}'"
+    system 'git push origin master'
+    sleep(3600)
   end
-  system 'git add csv/'
-  system "git commit -m 'updated from #{mv_web.first.datetime.to_s}'"
-  system 'git push origin master'
-  sleep(3600)
 end
